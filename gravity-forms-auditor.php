bd43947dc862d66dd7dfc20f8ea14eb7827949d8
Joseph Kerkhof <musicaljoeker@gmail.com>
2017-12-06 13:18:32 -0600

    Adding an tester AJAX call

--- gravity-forms-auditor.php
+++ gravity-forms-auditor.php
@@ -15,8 +15,40 @@ function gf_auditor_menu() {
 
 add_action( 'network_admin_menu', 'gf_auditor_menu' );
 
+// Registering an ajax action
+function my_action() {
+	global $wpdb; // this is how you get access to the database
+
+	$whatever = intval( $_POST['whatever'] );
+
+	$whatever += 10;
+
+    echo $whatever;
+
+	wp_die(); // this is required to terminate immediately and return a proper response
+}
+add_action( 'wp_ajax_my_action', 'my_action' );
+
 function gf_auditor() {
     ?>
+    <script type="text/javascript" >
+	jQuery(document).ready(function($) {
+
+		var data = {
+			'action': 'my_action',
+			'whatever': 1234
+		};
+
+		// since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
+        jQuery('#submit').click(function() {
+            console.log('#submit clicked');
+            jQuery.post(ajaxurl, data, function(response) {
+                console.log('Got this from the server: ' + response);
+            });
+        });
+		
+	});
+	</script> 
     <h1>Gravity Forms Auditor</h1>
     <?php
     submit_button( 'Run Report' );
