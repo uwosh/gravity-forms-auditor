1708a481e07b04b1fa63b3307b5b5cf69f651d37
Kerkhof, Joseph A <musicaljoeker@gmail.com>
2017-12-06 10:45:26 -0600

    WP plugin init

---
+++ gravity-forms-auditor.php
@@ -0,0 +1,9 @@
+/*
+Plugin Name: Gravity Forms Auditor
+Plugin URI: http://uwosh.edu/
+Description: Upon request, returns a report of the configuration of all Gravity Forms on a multisite.
+Author: Joseph Kerkhof
+Author URI: https://twitter.com/musicaljoeker
+Version: 0.1
+Network: True
+*/
