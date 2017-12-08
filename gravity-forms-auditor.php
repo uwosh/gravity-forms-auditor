542dde7582bec4413b772b333d7f964223e90748
Joseph Kerkhof <musicaljoeker@gmail.com>
2017-12-07 19:50:28 -0600

    changing the order

--- gravity-forms-auditor.php
+++ gravity-forms-auditor.php
@@ -47,12 +47,13 @@ function report_runner() {
         if( table_exists($gf_form_meta_tables[$i]) ){
             $query = "SELECT form_id, display_meta FROM " . $gf_form_meta_tables[$i];
             $rows = $wpdb->get_results( $query );
-            // $rows["display_meta"] = str_replace( "\\\"", "\"", $rows["display_meta"] );
+            array_unshift($rows, $i+1); // adding the site id to the beginning of the array
+            array_push( $forms_metadata, $rows );
+
+            // Converting the JSON coming back from the DB to an array
             for( $j=0; $j<count($rows); $j++ ) {
                 $rows[$j]->display_meta = json_decode( $rows[$j]->display_meta, true );
             }
-            array_unshift($rows, $i+1); // adding the site id to the beginning of the array
-            array_push( $forms_metadata, $rows );
         }else {
             continue;
         }
