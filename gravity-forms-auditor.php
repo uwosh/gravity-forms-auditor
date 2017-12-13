eeed55daeb39e338463c68ee4d3915b429013d00
Kerkhof, Joseph A <musicaljoeker@gmail.com>
2017-12-13 08:28:00 -0600

    Adding the last run to the page

--- gravity-forms-auditor.php
+++ gravity-forms-auditor.php
@@ -50,6 +50,13 @@ function gf_auditor() {
 	</script> 
     <h1>Gravity Forms Auditor</h1>
     <?php
+    global $wpdb;
+    if( table_exists( $wpdb->prefix . "form_auditor"; ) ){
+        $query = "SELECT last_run FROM " . $wpdb->prefix . "form_auditor ORDER BY last_run DESC LIMIT 1";
+        $result = $wpdb->get_results( $query );
+        $last_run = $result[0]->last_run;
+        echo 'Last run: ' . $last_run;
+    }
     submit_button( 'Run Report' );
 }
 
